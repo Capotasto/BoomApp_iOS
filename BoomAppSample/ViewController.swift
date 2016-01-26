e613ced86facdd89840a3a2cff1997b3e6cf9e7e
Capotasto <rocker519804@gmail.com>
2016-01-25 18:15:27 -0800

    first commit

--- BoomAppSample/ViewController.swift
+++ BoomAppSample/ViewController.swift
@@ -9,7 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var coolBlue: UIImageView!
+    @IBOutlet weak var coolRed: UIImageView!
+    
+    @IBOutlet weak var coolButtonBlue: UIButton!
+    @IBOutlet weak var coolButtonRed: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +26,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func hideBlue(sender: AnyObject) {
+        if (coolBlue.hidden == false) {
+            coolBlue.hidden = true
+            coolButtonBlue.setTitle("Show Blue Button", forState: .Normal)
 
+        }else{
+            coolBlue.hidden = false
+            coolButtonBlue.setTitle("Hide Blue Button", forState: .Normal)
+
+        }
+
+    }
+
+    @IBAction func hideRed(sender: AnyObject) {
+        if (coolRed.hidden == false) {
+            coolRed.hidden = true
+            coolButtonRed.setTitle("Show Red Button", forState: .Normal)
+            
+        }else{
+            coolRed.hidden = false
+            coolButtonRed.setTitle("Hide Red Button", forState: .Normal)
+            
+        }
+
+    }
 
 }
 
